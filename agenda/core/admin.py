ad4ac2f6788fb12ff562889a1082fc3df9c6a232
aline bastos <alinebastos23@outlook.com>
2020-11-13 22:00:42 -0300

    criacao/registro banco de dados

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from core.models import UserProfile
 # Register your models here.
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email')
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class UserProfile(models.Model):
+    SEXO_CHOICES = (
+        (u'F', u'Feminino'),
+        (u'M', u'Masculino')
+    )
+    nome = models.CharField(max_length=100)
+    data_nasc = models.DateTimeField
+    sexo = models.CharField(max_length=2, choices=SEXO_CHOICES)
+    cpf = models.CharField(max_length=11, primary_key=True)
+    endereco = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=11)
+    email = models.CharField(max_length=100)
+    user = models.OneToOneField('auth.User', unique=True, on_delete=models.CASCADE)
+
