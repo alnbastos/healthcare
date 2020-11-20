eea28df01f149422beb9a5ae66aa8d2565cd1876
aline bastos <alinebastos23@outlook.com>
2020-11-19 23:26:11 -0300

    Teste de requisição de página

--- agenda/core/tests.py
+++ agenda/core/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from core import views
 
-# Create your tests here.
+
+class cadastro_cliente_test(TestCase):
+    def test_cadastro_cliente(self):
+        # teste de requisição
+        request = RequestFactory().get('/cadastro/cliente/')
+        response = views.cadastro_cliente(request)
+        self.assertEqual(response.status_code, 400)
